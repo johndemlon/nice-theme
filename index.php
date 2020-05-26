8ccaab1907b8824bc5b168662e948dab4ce35cc3
the fake 8 <53253324+johndemlon@users.noreply.github.com>
2020-05-26 15:55:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("php/s1.php"); ?>
+<?php include("php/c1.php") ?>
+<html>
+<head>
+<meta name="google-site-verification" content="Roumg8Derx8SSWjH1sAQgFv_2oQVtUjkqoqDILjj1uY" />
+<meta name="description" content="Ethereum is a global, decentralized platform for money and new kinds of applications. On Ethereum, you can write code that controls money, and build applications accessible anywhere in the world.">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="a.css">
+<script src="a.js"></script>
+<link rel="shortcut icon" type="image/png" href="https://ethereum.org/favicon.png"/>
+<title>V</title>
+</head>
+<body>
+<nav>
+<a href="https://usus.ml" style="text-decoration: none;"><div style="border-style: solid;"><center>Main</center></div></a>
+<a href="https://usus.ml/tools" style="text-decoration: none;"><div style="border-style: solid;"><center>Tools</center></div></a>
+<div style="border-style: solid;"><center><div id="time"></div><script>function updateClock() {var now = new Date(),months = ['January', 'February', '...'];time = now.getHours() + ':' + now.getMinutes()+":" + now.getSeconds();date = [now.getDate(), months[now.getMonth()],now.getFullYear()].join(' ');document.getElementById('time').innerHTML = [date, time].join(' / ');setTimeout(updateClock, 1000);}updateClock();</script></center></div>
+<a href="#contact" style="text-decoration: none;"><div style="border-style: solid;"><center>Contact</center></div></a></nav>
+<center><video alt="Ethereum" width="380" height="380" src="https://ethereum.org/ethereum-hero-light.mp4" playsinline="" autoplay="autoplay" loop="loop" muted="muted" style="max-width:100%;height:auto;"></video></center><br>
+<center>the secret directory will be opened in :</center><br>
+<center><div id="demo"></div></center><br>
+<script>
+var countDownDate = new Date("May 27, 2020 0:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";
+if (distance < 0) {clearInterval(x);document.getElementById("demo").innerHTML = '<a href="https://usus.ml/img/">CLICK NOW</a>';}}, 1000);
+</script>
+<footer><div style="border-style: solid;" id="contact"><center>CONTACT</center><br><center>EMAIL : a@u5u5.ml</center></div></footer>
+</body></html>
---
+++ php/a.php
@@ -0,0 +1,6 @@
+<html>
+<head><meta charset="utf-8"/></head>
+<body>
+1. https://www.docpid.com/posts/ <a href="https://www.docpid.com/posts/">LINK</a> XSS - DONE
+</body>
+</html>
